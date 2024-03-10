66860b95e7dd42f4f2db7928bfa0cb5fc7512c41
booleto <exaccxd@gmail.com>
2024-03-10 21:58:48 +0700

    finished 1944

--- number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
+++ number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
@@ -25,15 +25,38 @@ from time import time_ns
 class Solution:
     def canSeePersonsCount(self, heights: List[int]) -> List[int]:
         result : List[int] = [0 for person in heights]
-        heights_len = len(heights)
-        for i in reversed(range(heights_len)):
-            for j in range(i+1, heights_len):
+        vision : deque = Deque()
+        count : int = len(heights)
 
-                result[i] += 1
-                if heights[i] <= heights[j]:
+        vision.append(count - 1)
+        for i in reversed(range(count-1)):
+            previous_vision = None
+            count = 0
+            while len(vision) != 0:
+                if heights[i] <= heights[vision[0]]:
+                    result[i] = count + 1
                     break
-                
+                else:
+                    count += 1
+                    previous_vision = vision.popleft() # current person blocks next visible person
+            else:
+                result[i] = count # i is taller than everyone in vision
+            vision.appendleft(i)
+        
         return result
+
+    # def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+    #     result : List[int] = [0 for person in heights]
+    #     heights_len = len(heights)
+    #     for i in reversed(range(heights_len)):
+    #         for j in range(i+1, heights_len):
+
+    #             result[i] += 1
+    #             if heights[i] <= heights[j]:
+    #                 break
+                
+    #     return result
 
 
 print(Solution().canSeePersonsCount([10,6,8,5,11,9]))
+print(Solution().canSeePersonsCount([5,1,2,3,10]))
